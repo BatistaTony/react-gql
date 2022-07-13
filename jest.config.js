0f79577b8c2a8b0b13111895c8daabb289fa475d
BatistaTony <batistatomeoliveira96@gmail.com>
2022-07-13 16:49:10 +0100

    chore: add a ci pipeline

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,8 @@ module.exports = {
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  testMatch:  [
+      '<rootDir>/src/**/*.(test).{js,jsx,ts,tsx}',
+      '<rootDir>/src/**/?(*.)(spec|test).{js,jsx,ts,tsx}'
+    ],
 }
